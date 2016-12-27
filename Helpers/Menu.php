729d40dad82f69d9a6269606292f6a05ca162785
Gkiokan Sali <gkiokan@hotmail.de>
2016-12-27 03:47:29 +0100

    A little comment and import of the Route Class

--- Helpers/Menu.php
+++ Helpers/Menu.php
@@ -1,10 +1,19 @@
 <?php
 namespace Gkiokan\Core\Helpers;
 
+use \Illuminate\Support\Facades\Route;
+
 class Menu {
 
+    /*
+        Current Route is trur | false
+        ----
+        Menu item is in current Path ?
+        Requires $name to be a valid Path name
+        Returns matching route true anything else false.
+    */
     public static function is($name=''){
-        $routeName =  \Illuminate\Support\Facades\Route::getFacadeRoot()->current()->getName();
+        $routeName = Route::getFacadeRoot()->current()->getName();
         return $routeName == $name ? true : false;
     }
 
