efe4034cb42466b4627e8eee65a5d3d22fdb7f11
Gkiokan Sali <gkiokan@hotmail.de>
2017-01-01 02:09:06 +0100

    update

--- Resources/views/partials/navigation/leftmenu.blade.php
+++ Resources/views/partials/navigation/leftmenu.blade.php
@@ -1,3 +1,5 @@
 {{-- List all Side Menu elements here uppon  --}}
 @include('profile::partials.navigation.leftmenu')
 @include('company::partials.navigation.leftmenu')
+
+@include('secondhandshop::partials.navigation.leftmenu')
